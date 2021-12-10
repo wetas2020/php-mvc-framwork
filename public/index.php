38cf52123349315cc1342342f919dbe594707e28
wetas2020 <62056784+wetas2020@users.noreply.github.com>
2021-12-10 16:25:14 +0100

    Start working on Auth(register)

--- public/index.php
+++ public/index.php
@@ -2,6 +2,7 @@
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
+use app\controllers\AuthController;
 use app\controllers\SiteController;
 use app\core\Application;
 
@@ -11,5 +12,10 @@ $app->router->get('/', [SiteController::class, 'home']);
 //$app->router->get('/contact', 'contact');
 $app->router->get('/contact', [SiteController::class, 'contact']);
 $app->router->post('/contact', [SiteController::class, 'handleContact']);
+
+$app->router->get('/login', [AuthController::class, 'login']);
+$app->router->post('/login', [AuthController::class, 'login']);
+$app->router->get('/register', [AuthController::class, 'register']);
+$app->router->post('/register', [AuthController::class, 'register']);
 
 $app->run();
---
+++ views/register.php
@@ -0,0 +1,30 @@
+<h1>Create an account</h1>
+<form action="" method="post">
+    <div class="row">
+        <div class="col">
+            <div class="mb-3">
+                <label>Firstname</label>
+                <input type="text" class="form-control" name="firstname">
+            </div>
+        </div>
+        <div class="col">
+            <div class="mb-3">
+                <label>Lastname</label>
+                <input type="text" class="form-control" name="lastname">
+            </div>
+        </div>
+    </div>
+    <div class="mb-3">
+        <label>Email</label>
+        <input type="text" class="form-control" name="email">
+    </div>
+    <div class="mb-3">
+        <label>Password</label>
+        <input type="password" class="form-control" name="password">
+    </div>
+    <div class="mb-3">
+        <label>Confirm Password</label>
+        <input type="password" class="form-control" name="confirmPassword">
+    </div>
+    <button type="submit" class="btn btn-primary">Submit</button>
+</form>
