875ece8ad442c31648b82616b84671228ada5f4f
wetas2020 <62056784+wetas2020@users.noreply.github.com>
2021-12-12 20:13:13 +0100

    Implement validation

--- models/RegisterModel.php
+++ models/RegisterModel.php
@@ -12,9 +12,20 @@ class RegisterModel extends Model
     public string $confirmPassword;
     public string $email;
 
+
     public function register()
     {
         echo "Creating new user...";
     }
 
+    public function rules(): array
+    {
+        return [
+            'firstname' => [self::RULE_REQUIRED],
+            'lastname' => [self::RULE_REQUIRED],
+            'email' => [self::RULE_REQUIRED, self::RULE_EMAIL],
+            'password' => [self::RULE_REQUIRED, [self::RULE_MIN, 'min' => 8]],
+            'confirmPassword' => [self::RULE_REQUIRED, [self::RULE_MATCH, 'match' => 'password']],
+        ];
+    }
 }
